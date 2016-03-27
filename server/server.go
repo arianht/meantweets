1827fcb4105314a3f0d92db8d2bc3f87ab863358
Cory Harasha <cory.harasha@gmail.com>
2016-03-26 23:38:19 -0700

    Add a basic go web server

---
+++ server/server.go
@@ -0,0 +1,25 @@
+/*
+The main web server for Mean Tweets. Requires App Engine.
+
+Run locally with:
+goapp server github.com/arianht/meantweets/server
+
+It will automatically detect changes in the code.
+
+Push to the cloud with:
+appcfg.py A <project_id> -V <version> update github.com/arianht/meantweets/server
+*/
+package server
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+func handler(writer http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(writer, "Mean Tweets basic server!")
+}
