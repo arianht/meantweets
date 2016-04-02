008987952df156f1574f15e44344f6033ce4b28a
Cory Harasha <cory.harasha@gmail.com>
2016-04-01 22:40:52 -0700

    Update the SentimentAnalyzer interface and provide a mock

--- crawl/sentiment.go
+++ crawl/sentiment.go
@@ -1,5 +1,19 @@
+/*
+Package for crawling for tweets, getting their sentiment score, and writing them
+to the database.
+*/
 package crawl
 
-type AnalyzeSentiment interface {
+// The interface for retrieving the sentiment score of a tweet.
+type SentimentAnalyzer interface {
 	GetScoreForTweet(tweet string) int
+}
+
+// Provide a mock Dao for unit tests of files that depend on a Dao.
+type sentimentAnalyzerMock struct {
+	scores map[string]int
+}
+
+func (analyzer sentimentAnalyzerMock) GetScoreForTweet(tweet string) int {
+	return analyzer.scores[tweet]
 }
