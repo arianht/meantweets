356e1f0914a352803b8e8060b6f5d380b556b286
Cory Harasha <cory.harasha@gmail.com>
2016-04-07 19:54:11 -0700

    Clean up crawler_test to follow the go cases loop.
    This makes it easy to extend to multiple test cases.

--- crawl/crawler_test.go
+++ crawl/crawler_test.go
@@ -8,52 +8,66 @@ import (
 	"github.com/kurrik/twittergo"
 )
 
-func TestCrawlBasicFlowWithEmptyDatabase(t *testing.T) {
-	dao := database.DaoMock{Tweets: &[]database.Tweet{}}
-	twitter := TwitterFacadeMock{
-		Tweets: map[string][]twittergo.Tweet{
-			"Johnny": []twittergo.Tweet{
-				twittergo.Tweet{"id_str": "0", "text": "j_one"},
-				twittergo.Tweet{"id_str": "1", "text": "j_two"},
+func TestCrawl(t *testing.T) {
+	testCases := []struct {
+		initialDatabase     []database.Tweet
+		celebrities         []string
+		tweets              map[string][]twittergo.Tweet
+		scores              map[string]int32
+		expectedEndDatabase []database.Tweet
+	}{
+		{ // Test Case: Empty Initial Database
+			initialDatabase: []database.Tweet{},
+			celebrities:     []string{"Johnny", "Emma"},
+			tweets: map[string][]twittergo.Tweet{
+				"Johnny": []twittergo.Tweet{
+					twittergo.Tweet{"id_str": "0", "text": "j_one"},
+					twittergo.Tweet{"id_str": "1", "text": "j_two"},
+				},
+				"Emma": []twittergo.Tweet{
+					twittergo.Tweet{"id_str": "2", "text": "e_one"},
+				},
 			},
-			"Emma": []twittergo.Tweet{
-				twittergo.Tweet{"id_str": "2", "text": "e_one"},
+			scores: map[string]int32{
+				"j_one": 2,
+				"j_two": 100,
+				"e_one": 872,
+			},
+			expectedEndDatabase: []database.Tweet{
+				database.Tweet{
+					CelebrityName: "Johnny",
+					Id:            0,
+					Score:         2,
+				},
+				database.Tweet{
+					CelebrityName: "Johnny",
+					Id:            1,
+					Score:         100,
+				},
+				database.Tweet{
+					CelebrityName: "Emma",
+					Id:            2,
+					Score:         872,
+				},
 			},
 		},
-	}
-	sentiment := SentimentAnalyzerMock{
-		Scores: map[string]int32{
-			"j_one": 2,
-			"j_two": 100,
-			"e_one": 872,
-		},
-	}
-	twitterCrawler := TwitterCrawler{
-		Dao:       dao,
-		Twitter:   twitter,
-		Sentiment: sentiment,
 	}
 
-	twitterCrawler.Crawl([]string{"Johnny", "Emma"})
+	for _, testCase := range testCases {
+		dao := database.DaoMock{Tweets: &testCase.initialDatabase}
+		twitter := TwitterFacadeMock{Tweets: testCase.tweets}
+		sentiment := SentimentAnalyzerMock{Scores: testCase.scores}
+		twitterCrawler := TwitterCrawler{
+			Dao:       dao,
+			Twitter:   twitter,
+			Sentiment: sentiment,
+		}
 
-	expectedTweetsDatabase := []database.Tweet{
-		database.Tweet{
-			CelebrityName: "Johnny",
-			Id:            0,
-			Score:         2,
-		},
-		database.Tweet{
-			CelebrityName: "Johnny",
-			Id:            1,
-			Score:         100,
-		},
-		database.Tweet{
-			CelebrityName: "Emma",
-			Id:            2,
-			Score:         872,
-		},
-	}
-	if !reflect.DeepEqual(*dao.Tweets, expectedTweetsDatabase) {
-		t.Errorf("Expected tweet database is %v, but was %v", expectedTweetsDatabase, *dao.Tweets)
+		twitterCrawler.Crawl(testCase.celebrities)
+
+		if !reflect.DeepEqual(*dao.Tweets, testCase.expectedEndDatabase) {
+			t.Errorf("Expected tweet database is %v, but was %v", testCase.expectedEndDatabase,
+				*dao.Tweets)
+		}
 	}
 }
