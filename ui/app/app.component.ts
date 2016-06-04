067c2eec950471ad7310aa9baba01037eedabb49
Arian Hatefi <hatefi.arian@gmail.com>
2016-06-03 23:04:30 -0700

    Add a Material 2 button example

--- ui/app/app.component.ts
+++ ui/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
 
 @Component({
+  directives: [MD_BUTTON_DIRECTIVES],
   selector: 'my-app',
   // styles: [
     // require('normalize.css'),
   // ],
   template: `
     <h1>Hello World!</h1>
+    <button md-raised-button>Test Button!</button>
   `
 })
 export class AppComponent {
